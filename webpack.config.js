34c5d4bcd73a21d82f86c3d110b1f2c901b81b1f
Adam Meszaros <adam.meszaros@digirati.com>
2019-04-01 17:26:34 +0100

    fix the concat issue with the IIIF Explorer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const Config = require('webpack-config').default;
 const webpack = require('webpack');
-
-module.exports = new Config().extend('@fesk/scripts/webpack').merge({
+const conf = new Config().extend('@fesk/scripts/webpack').merge({
   plugins: [
     new webpack.EnvironmentPlugin({
       COLLECTION_SERVER: 'https://iiif-collection.ch.digtest.co.uk/p3/',
     }),
   ],
 });
+
+conf.plugins[0].opts.metalsmithDest = './assets';
+
+module.exports = conf;
